5f900bf9667b4e46f4ffac17c7ae1a69700c974c
John Forstmeier <john.forstmeier@heavywater.com>
2019-09-14 09:49:53 -0400

    Build initial table package/test suite

--- table/table.go
+++ table/table.go
@@ -1,1 +1,139 @@
 package table
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
+
+type dynamoDBClient interface {
+	PutItem(input *dynamodb.PutItemInput) (*dynamodb.PutItemOutput, error)
+	GetItem(input *dynamodb.GetItemInput) (*dynamodb.GetItemOutput, error)
+	DeleteItem(input *dynamodb.DeleteItemInput) (*dynamodb.DeleteItemOutput, error)
+}
+
+// Table provides helper methods for persisting/retrieving/deleting items
+type Table interface {
+	Add(table string, items ...string) error
+	Get(table string, items ...string) (bool, error)
+	Remove(table string, item ...string) error
+}
+
+// Client implements the Table interface
+type Client struct {
+	dynamodb dynamoDBClient
+}
+
+// New generates a Table implementation with an active client
+func New() Table {
+	return &Client{
+		dynamodb: dynamodb.New(session.New()),
+	}
+}
+
+// Add puts a new item into DynamoDB
+func (c *Client) Add(table string, items ...string) error {
+	input := &dynamodb.PutItemInput{}
+
+	if table == "subscribers" {
+		input = &dynamodb.PutItemInput{
+			Item: map[string]*dynamodb.AttributeValue{
+				"query": {
+					S: aws.String(items[0]),
+				},
+				"subname": {
+					S: aws.String(items[1]),
+				},
+				"target": {
+					S: aws.String(items[2]),
+				},
+			},
+			TableName: aws.String(table),
+		}
+	} else if table == "queries" {
+		input = &dynamodb.PutItemInput{
+			Item: map[string]*dynamodb.AttributeValue{
+				"query": {
+					S: aws.String(items[0]),
+				},
+			},
+			TableName: aws.String(table),
+		}
+	}
+
+	_, err := c.dynamodb.PutItem(input)
+	if err != nil {
+		return fmt.Errorf("put item error: %s", err.Error())
+	}
+	return nil
+}
+
+// Get retrieves an item from DynamoDB
+func (c *Client) Get(table string, items ...string) (bool, error) {
+	input := &dynamodb.GetItemInput{}
+
+	if table == "subscribers" {
+		input = &dynamodb.GetItemInput{
+			Key: map[string]*dynamodb.AttributeValue{
+				"query": {
+					S: aws.String(items[0]),
+				},
+				"name": {
+					S: aws.String(items[1]),
+				},
+			},
+			TableName: aws.String(table),
+		}
+	} else if table == "queries" {
+		input = &dynamodb.GetItemInput{
+			Key: map[string]*dynamodb.AttributeValue{
+				"query": {
+					S: aws.String(items[0]),
+				},
+			},
+			TableName: aws.String(table),
+		}
+	}
+
+	_, err := c.dynamodb.GetItem(input)
+	if err != nil {
+		return false, fmt.Errorf("get item error: %s", err.Error())
+	}
+	return true, nil
+}
+
+// Remove deletes an item from DynamoDB
+func (c *Client) Remove(table string, items ...string) error {
+	input := &dynamodb.DeleteItemInput{}
+
+	if table == "subscribers" {
+		input = &dynamodb.DeleteItemInput{
+			Key: map[string]*dynamodb.AttributeValue{
+				"query": {
+					S: aws.String(items[0]),
+				},
+				"name": {
+					S: aws.String(items[1]),
+				},
+			},
+			TableName: aws.String(table),
+		}
+	} else if table == "queries" {
+		input = &dynamodb.DeleteItemInput{
+			Key: map[string]*dynamodb.AttributeValue{
+				"query": {
+					S: aws.String(items[0]),
+				},
+			},
+			TableName: aws.String(table),
+		}
+	}
+
+	_, err := c.dynamodb.DeleteItem(input)
+	if err != nil {
+		return fmt.Errorf("delete item error: %s", err.Error())
+	}
+	return nil
+}
--- table/table_test.go
+++ table/table_test.go
@@ -1,1 +1,94 @@
 package table
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
+
+type tableMock struct {
+	putItemOutput    *dynamodb.PutItemOutput
+	putItemError     error
+	getItemOutput    *dynamodb.GetItemOutput
+	getItemError     error
+	deleteItemOutput *dynamodb.DeleteItemOutput
+	deleteItemError  error
+}
+
+func (mock *tableMock) PutItem(input *dynamodb.PutItemInput) (*dynamodb.PutItemOutput, error) {
+	return mock.putItemOutput, mock.putItemError
+}
+
+func (mock *tableMock) GetItem(input *dynamodb.GetItemInput) (*dynamodb.GetItemOutput, error) {
+	return mock.getItemOutput, mock.getItemError
+}
+
+func (mock *tableMock) DeleteItem(input *dynamodb.DeleteItemInput) (*dynamodb.DeleteItemOutput, error) {
+	return mock.deleteItemOutput, mock.deleteItemError
+}
+
+func TestAdd(t *testing.T) {
+	tests := []struct {
+		desc          string
+		table         string
+		items         []string
+		putItemOutput *dynamodb.PutItemOutput // kept for possible method expansion
+		putItemError  error
+		err           string
+	}{
+		{
+			desc:  "put item error",
+			table: "queries",
+			items: []string{
+				"query",
+			},
+			putItemOutput: nil,
+			putItemError:  errors.New("mock put error"),
+			err:           "put item error: mock put error",
+		},
+		{
+			desc:  "successful subscribers invocation",
+			table: "subscribers",
+			items: []string{
+				"query",
+				"subname",
+				"target",
+			},
+			putItemOutput: nil,
+			putItemError:  nil,
+			err:           "",
+		},
+		{
+			desc:  "successful queries invocation",
+			table: "queries",
+			items: []string{
+				"query",
+			},
+			putItemOutput: nil,
+			putItemError:  nil,
+			err:           "",
+		},
+	}
+
+	for _, test := range tests {
+		c := &Client{
+			dynamodb: &tableMock{
+				putItemOutput: test.putItemOutput,
+				putItemError:  test.putItemError,
+			},
+		}
+
+		if err := c.Add(test.table, test.items...); err != nil && err.Error() != test.err {
+			t.Errorf("description: %s, error received: %s, expected: %s", test.desc, err.Error(), test.err)
+		}
+	}
+}
+
+func TestGet(t *testing.T) {
+
+}
+
+func TestRemove(t *testing.T) {
+
+}
