f00fb0854178412c8eafd1487db3fdcdc3775104
John Forstmeier <john.forstmeier@heavywater.com>
2019-09-15 11:02:21 -0400

    Build initial create method resources

--- query/query.go
+++ query/query.go
@@ -1,51 +1,90 @@
 package query
 
 import (
-        "github.com/aws/aws-lambda-go/events"
+	"fmt"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/events"
+
+	"github.com/forstmeier/comana/storage"
+	"github.com/forstmeier/comana/table"
 )
 
-func Create(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+// Create adds a query to S3 for periodic execution
+func Create(request events.APIGatewayProxyRequest, table table.Table, storage storage.Storage) (events.APIGatewayProxyResponse, error) {
+	queryName := request.QueryStringParameters["query_name"]
 
-        // outline:
-        // [ ] parse file from request
-        // [ ] parse query name from request
-        // [ ] check if query exists in dynamodb
-        // - [ ] true:
-        // - - [ ] return error
-        // - [ ] false:
-        // - - [ ] add query name to dynamodb
-        // - - [ ] persist query file to s3
-        // [ ] return success
+	check, err := table.Get(queryName)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode:      500,
+			Body:            "error getting query table: " + err.Error(),
+			IsBase64Encoded: false,
+		}, fmt.Errorf("error getting query table: %s", err.Error())
+	}
 
+	if check == false {
+		if err := table.Add("queries", queryName); err != nil {
+			return events.APIGatewayProxyResponse{
+				StatusCode:      500,
+				Body:            "error creating query: " + err.Error(),
+				IsBase64Encoded: false,
+			}, fmt.Errorf("error creating query: %s", err.Error())
+		}
+
+		if err := storage.PutFile(queryName, strings.NewReader(request.Body)); err != nil {
+			return events.APIGatewayProxyResponse{
+				StatusCode:      500,
+				Body:            "error putting query file: " + err.Error(),
+				IsBase64Encoded: false,
+			}, fmt.Errorf("error putting query file: %s", err.Error())
+		}
+	} else {
+		return events.APIGatewayProxyResponse{
+			StatusCode:      200,
+			Body:            "query already exists",
+			IsBase64Encoded: false,
+		}, nil
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode:      200,
+		Body:            "success",
+		IsBase64Encoded: false,
+	}, nil
 }
 
+// Run executes all stored queries and returns results to subscribers
 func Run() (events.APIGatewayProxyResponse, error) {
 
-        // outline:
-        // [ ] read in all queries from s3
-        // [ ] loop over queries
-        // - [ ] read in subscribers to query from dynamodb
-        // - [ ] run query on bq
-        // - [ ] parse results to response json
-        // - [ ] post results to webhook
-        // [ ] return success
+	// outline:
+	// [ ] read in all queries from s3
+	// [ ] loop over queries
+	// - [ ] read in subscribers to query from dynamodb
+	// - [ ] run query on bq
+	// - [ ] parse results to response json
+	// - [ ] post results to webhook
+	// [ ] return success
 
+	return events.APIGatewayProxyResponse{}, nil
 }
 
+// Delete removes a query from S3 - internal use only
 func Delete(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
-        // outline:
-        // [ ] parse secret from request
-        // [ ] parse query name from request
-        // [ ] check if secret is valid
-        // - [ ] true: continue
-        // - [ ] false: return error
-        // [ ] check if query exists in dynamodb
-        // - [ ] true:
-        // - - [ ] delete file from s3
-        // - - [ ] delete query name from dynamodb
-        // - [ ] false:
-        // - - [ ] return error
-        // [ ] return success
+	// outline:
+	// [ ] parse secret from request
+	// [ ] parse query name from request
+	// [ ] check if secret is valid
+	// - [ ] true: continue
+	// - [ ] false: return error
+	// [ ] check if query exists in dynamodb
+	// - [ ] true:
+	// - - [ ] delete file from s3
+	// - - [ ] delete query name from dynamodb
+	// - [ ] false:
+	// - - [ ] return error
+	// [ ] return success
 
+	return events.APIGatewayProxyResponse{}, nil
 }
--- query/query_test.go
+++ query/query_test.go
@@ -1,1 +1,154 @@
 package query
+
+import (
+	"errors"
+	"io"
+	"testing"
+
+	"github.com/aws/aws-lambda-go/events"
+)
+
+type mockTable struct {
+	getOutput bool
+	getErr    error
+	addErr    error
+	removeErr error
+}
+
+func (mock *mockTable) Get(table string, items ...string) (bool, error) {
+	return mock.getOutput, mock.getErr
+}
+
+func (mock *mockTable) Add(table string, items ...string) error {
+	return mock.addErr
+}
+
+func (mock *mockTable) Remove(table string, item ...string) error {
+	return mock.removeErr
+}
+
+type mockStorage struct {
+	putErr      error
+	getOutput   io.Reader
+	getErr      error
+	pathsOutput []string
+	pathsErr    error
+}
+
+func (mock *mockStorage) PutFile(key string, file io.Reader) error {
+	return mock.putErr
+}
+
+func (mock *mockStorage) GetFile(string) (io.Reader, error) {
+	return mock.getOutput, mock.getErr
+}
+
+func (mock *mockStorage) GetPaths() ([]string, error) {
+	return mock.pathsOutput, mock.pathsErr
+}
+
+func TestCreate(t *testing.T) {
+	tests := []struct {
+		desc      string
+		req       events.APIGatewayProxyRequest
+		getOutput bool
+		getErr    error
+		addErr    error
+		putErr    error
+		status    int
+		err       string
+	}{
+		{
+			desc: "table get error",
+			req: events.APIGatewayProxyRequest{
+				QueryStringParameters: map[string]string{
+					"query_name": "test-name",
+				},
+			},
+			getOutput: false,
+			getErr:    errors.New("mock table get error"),
+			addErr:    nil,
+			putErr:    nil,
+			status:    500,
+			err:       "error getting query table: mock table get error",
+		},
+		{
+			desc: "table add error",
+			req: events.APIGatewayProxyRequest{
+				QueryStringParameters: map[string]string{
+					"query_name": "test-name",
+				},
+			},
+			getOutput: false,
+			getErr:    nil,
+			addErr:    errors.New("mock table add error"),
+			putErr:    nil,
+			status:    500,
+			err:       "error creating query: mock table add error",
+		},
+		{
+			desc: "table add error",
+			req: events.APIGatewayProxyRequest{
+				QueryStringParameters: map[string]string{
+					"query_name": "test-name",
+				},
+			},
+			getOutput: false,
+			getErr:    nil,
+			addErr:    nil,
+			putErr:    errors.New("mock storage put error"),
+			status:    500,
+			err:       "error putting query file: mock storage put error",
+		},
+		{
+			desc: "table exists",
+			req: events.APIGatewayProxyRequest{
+				QueryStringParameters: map[string]string{
+					"query_name": "test-name",
+				},
+			},
+			getOutput: true,
+			getErr:    nil,
+			addErr:    nil,
+			putErr:    nil,
+			status:    200,
+			err:       "",
+		},
+		{
+			desc: "successful invocation",
+			req: events.APIGatewayProxyRequest{
+				QueryStringParameters: map[string]string{
+					"query_name": "test-name",
+				},
+			},
+			getOutput: false,
+			getErr:    nil,
+			addErr:    nil,
+			putErr:    nil,
+			status:    200,
+			err:       "",
+		},
+	}
+
+	for _, test := range tests {
+		tbl := &mockTable{
+			getOutput: test.getOutput,
+			getErr:    test.getErr,
+			addErr:    test.addErr,
+		}
+
+		stg := &mockStorage{
+			putErr: test.putErr,
+		}
+
+		resp, err := Create(test.req, tbl, stg)
+
+		if err != nil && err.Error() != test.err {
+			t.Errorf("description: %s, error received: %s, expected: %s", test.desc, err.Error(), test.err)
+		}
+
+		if resp.StatusCode != test.status {
+			t.Errorf("description: %s, status received: %d, expected: %d", test.desc, resp.StatusCode, test.status)
+		}
+	}
+}
