9268571b7a008973696bcca78884493c5c9577ba
John Forstmeier <john.forstmeier@heavywater.com>
2019-09-22 22:12:24 -0400

    Build initial subscriber method

--- subscription/subscription.go
+++ subscription/subscription.go
@@ -1,35 +1,69 @@
 package subscription
 
 import (
-        "github.com/aws/aws-lambda-go/events"
+	"encoding/json"
+	"errors"
+	"net/url"
+
+	"github.com/aws/aws-lambda-go/events"
+
+	"github.com/forstmeier/quehook/table"
 )
 
-func Subscribe(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func createResponse(code int, msg string) (events.APIGatewayProxyResponse, error) {
+	resp := events.APIGatewayProxyResponse{
+		StatusCode:      code,
+		Body:            msg,
+		IsBase64Encoded: false,
+	}
 
-        // outline:
-        // [ ] parse query name from request
-        // [ ] parse webhook target / email target values from request
-        // [ ] check if target values are invalid
-        // - [ ] true: return error
-        // - [ ] false: continue
-        // [ ] check if query exists in dynamodb
-        // - [ ] true:
-        // - - [ ] add name to subscription table in dynamodb
-        // - - [ ] add target values to subscription table in dynamodb
-        // - [ ] false: return error
-        // [ ] return success
-        
+	if msg == "success" {
+		return resp, nil
+	}
+
+	return resp, errors.New(msg)
 }
 
-func Unsubscribe(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-
-        // outline:
-        // [ ] parse query name from request
-        // [ ] check if name exists in dynamodb subscription table
-        // - [ ] true:
-        // - - [ ] delete item from dynamodb subscription table
-        // - [ ] false:
-        // - - [ ] return error
-        // [ ] return success
-
+type sub struct {
+	QueryName        string `json:"query_name"`
+	SubscriberEmail  string `json:"subscriber_email"`
+	SubscriberName   string `json:"subscriber_name"`
+	SubscriberTarget string `json:"subscriber_target"`
 }
+
+// Subscribe adds a new subscriber to webhook query events
+func Subscribe(request events.APIGatewayProxyRequest, t table.Table) (events.APIGatewayProxyResponse, error) {
+	s := sub{}
+	if err := json.Unmarshal([]byte(request.Body), &s); err != nil {
+		return createResponse(400, "error parsing request: "+err.Error())
+	}
+
+	if _, err := url.ParseRequestURI(s.SubscriberTarget); err != nil {
+		return createResponse(400, "error parsing url: "+err.Error())
+	}
+
+	output, err := t.Get("queries", s.QueryName)
+	if err != nil {
+		return createResponse(500, "error getting query: "+err.Error())
+	} else if len(output) == 0 {
+		return createResponse(500, "no output returned")
+	}
+
+	if err := t.Add(s.QueryName, s.SubscriberEmail, s.SubscriberName, s.SubscriberTarget); err != nil {
+		return createResponse(500, "error adding subscriber: "+err.Error())
+	}
+
+	return createResponse(200, "success")
+}
+
+// Unsubscribe removes a subscriber from webhook query events
+// func Unsubscribe(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+// outline:
+// [ ] parse query name from request
+// [ ] check if name exists in dynamodb subscription table
+// - [ ] true:
+// - - [ ] delete item from dynamodb subscription table
+// - [ ] false:
+// - - [ ] return error
+// [ ] return success
+// }
--- subscription/subscription_test.go
+++ subscription/subscription_test.go
@@ -1,1 +1,161 @@
 package subscription
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/aws/aws-lambda-go/events"
+)
+
+func Test_createResponse(t *testing.T) {
+	tests := []struct {
+		desc string
+		code int
+		msg  string
+	}{
+		{
+			desc: "error message",
+			code: 500,
+			msg:  "failure",
+		},
+		{
+			desc: "success message",
+			code: 200,
+			msg:  "success",
+		},
+	}
+
+	for _, test := range tests {
+		resp, _ := createResponse(test.code, test.msg)
+
+		if resp.Body != test.msg {
+			t.Errorf("description: %s, body received: %s, expected: %s", test.desc, resp.Body, test.msg)
+		}
+
+		if resp.StatusCode != test.code {
+			t.Errorf("description: %s, status received: %d, expected: %d", test.desc, resp.StatusCode, test.code)
+		}
+	}
+}
+
+type mockTable struct {
+	getOutput []string
+	getErr    error
+	addErr    error
+	removeErr error
+}
+
+func (mock *mockTable) Get(table string, key string) ([]string, error) {
+	return mock.getOutput, mock.getErr
+}
+
+func (mock *mockTable) Add(table string, items ...string) error {
+	return mock.addErr
+}
+
+func (mock *mockTable) Remove(table string, key string) error {
+	return mock.removeErr
+}
+
+func TestSubscribe(t *testing.T) {
+	tests := []struct {
+		desc      string
+		req       events.APIGatewayProxyRequest
+		getOutput []string
+		getErr    error
+		addErr    error
+		code      int
+		err       string
+	}{
+		{
+			desc: "unmarshalling request body error",
+			req: events.APIGatewayProxyRequest{
+				Body: `["c-3po", "r2-d2"]`,
+			},
+			getOutput: nil,
+			getErr:    nil,
+			addErr:    nil,
+			code:      400,
+			err:       "error parsing request: json: cannot unmarshal array into Go value of type subscription.sub",
+		},
+		{
+			desc: "incorrect url target",
+			req: events.APIGatewayProxyRequest{
+				Body: `{"subscriber_target":"liesofthejedi.com"}`,
+			},
+			getOutput: nil,
+			getErr:    nil,
+			addErr:    nil,
+			code:      400,
+			err:       "error parsing url: parse liesofthejedi.com: invalid URI for request",
+		},
+		{
+			desc: "error getting query",
+			req: events.APIGatewayProxyRequest{
+				Body: `{"subscriber_target":"https://tragedyofdarthplagueisthewise.com"}`,
+			},
+			getOutput: nil,
+			getErr:    errors.New("mock get error"),
+			addErr:    nil,
+			code:      500,
+			err:       "error getting query: mock get error",
+		},
+		{
+			desc: "error getting query output",
+			req: events.APIGatewayProxyRequest{
+				Body: `{"subscriber_target":"https://jediarchives.com/kamino"}`,
+			},
+			getOutput: []string{},
+			getErr:    nil,
+			addErr:    nil,
+			code:      500,
+			err:       "no output returned",
+		},
+		{
+			desc: "add subscriber error",
+			req: events.APIGatewayProxyRequest{
+				Body: `{"query_name":"kamino","subscriber_target":"https://kenobi.io","subscriber_name":"obi-wan","subscriber_email":"kenobi@jedi.ord"}`,
+			},
+			getOutput: []string{"nu"},
+			getErr:    nil,
+			addErr:    errors.New("mock add error"),
+			code:      500,
+			err:       "error adding subscriber: mock add error",
+		},
+		{
+			desc: "successful invocation",
+			req: events.APIGatewayProxyRequest{
+				Body: `{"query_name":"kamino","subscriber_target":"https://kenobi.io","subscriber_name":"obi-wan","subscriber_email":"kenobi@jedi.ord"}`,
+			},
+			getOutput: []string{"dexter-jetster"},
+			getErr:    nil,
+			addErr:    nil,
+			code:      200,
+			err:       "",
+		},
+	}
+
+	for _, test := range tests {
+
+		tbl := &mockTable{
+			getOutput: test.getOutput,
+			getErr:    test.getErr,
+			addErr:    test.addErr,
+		}
+
+		resp, err := Subscribe(test.req, tbl)
+
+		if err != nil && err.Error() != test.err {
+			t.Errorf("description: %s, error received: %s, expected: %s", test.desc, err.Error(), test.err)
+		}
+
+		if resp.StatusCode != test.code {
+			t.Errorf("description: %s, status received: %d, expected: %d", test.desc, resp.StatusCode, test.code)
+		}
+
+	}
+}
+
+func TestUnsubscribe(t *testing.T) {
+
+}
