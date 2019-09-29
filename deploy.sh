e2c9de9b0369c5aa6584808c03fd3c2e25690a76
John Forstmeier <john.forstmeier@heavywater.com>
2019-09-29 15:41:12 -0400

    Fix incorrect flag in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-go build -ldflags "-X main.HANDLER=CREATE" -l querycreate
+go build -ldflags "-X main.HANDLER=CREATE" -o querycreate
 zip quehook-querycreate.zip querycreate
 aws lambda update-function-code --function-name quehook-query-create --zip-file fileb://quehook-querycreate.zip --region us-east-1
 
-go build -ldflags "-X main.HANDLER=RUN" -l queryrun
+go build -ldflags "-X main.HANDLER=RUN" -o queryrun
 zip quehook-queryrun.zip queryrun
 aws lambda update-function-code --function-name quehook-query-run --zip-file fileb://quehook-queryrun.zip --region us-east-1
 
-go build -ldflags "-X main.HANDLER=DELETE" -l querydelete
+go build -ldflags "-X main.HANDLER=DELETE" -o querydelete
 zip quehook-querydelete.zip querydelete
 aws lambda update-function-code --function-name quehook-query-delete --zip-file fileb://quehook-querydelete.zip --region us-east-1
 
-go build -ldflags "-X main.HANDLER=SUBSCRIBE" -l querysubscribe
+go build -ldflags "-X main.HANDLER=SUBSCRIBE" -o querysubscribe
 zip quehook-querysubscribe.zip querysubscribe
 aws lambda update-function-code --function-name quehook-subscription-subscribe --zip-file fileb://quehook-querysubscribe.zip --region us-east-1
 
-go build -ldflags "-X main.HANDLER=UNSUBSCRIBE" -l queryunsubscribe
+go build -ldflags "-X main.HANDLER=UNSUBSCRIBE" -o queryunsubscribe
 zip quehook-queryunsubscribe.zip queryunsubscribe
 aws lambda update-function-code --function-name quehook-subscription-unsubscribe --zip-file fileb://quehook-queryunsubscribe.zip --region us-east-1
