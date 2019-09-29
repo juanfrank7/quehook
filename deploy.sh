b77740fc781c6044b812d3b3a1c0807582eac485
John Forstmeier <john.forstmeier@heavywater.com>
2019-09-29 15:34:08 -0400

    Update main/deploy files

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-# go build -ldflags "-X main.HANDLER=SAVE" -o lambdasave
-# zip quehook-save.zip lambdasave
-# aws lambda update-function-code --function-name quehook-save --zip-file fileb://quehook-save.zip --region us-east-1
-#
-# go build -ldflags "-X main.HANDLER=LOAD" -o lambdaload
-# zip quehook-load.zip lambdaload
-# aws lambda update-function-code --function-name quehook-load --zip-file fileb://quehook-load.zip --region us-east-1
-#
-# go build -ldflags "-X main.HANDLER=BACKFILL" -o lambdabackfill
-# zip quehook-backfill.zip lambdabackfill
-# aws lambda update-function-code --function-name quehook-backfill --zip-file fileb://quehook-backfill.zip --region us-east-1
+
+go build -ldflags "-X main.HANDLER=CREATE" -l querycreate
+zip quehook-querycreate.zip querycreate
+aws lambda update-function-code --function-name quehook-query-create --zip-file fileb://quehook-querycreate.zip --region us-east-1
+
+go build -ldflags "-X main.HANDLER=RUN" -l queryrun
+zip quehook-queryrun.zip queryrun
+aws lambda update-function-code --function-name quehook-query-run --zip-file fileb://quehook-queryrun.zip --region us-east-1
+
+go build -ldflags "-X main.HANDLER=DELETE" -l querydelete
+zip quehook-querydelete.zip querydelete
+aws lambda update-function-code --function-name quehook-query-delete --zip-file fileb://quehook-querydelete.zip --region us-east-1
+
+go build -ldflags "-X main.HANDLER=SUBSCRIBE" -l querysubscribe
+zip quehook-querysubscribe.zip querysubscribe
+aws lambda update-function-code --function-name quehook-subscription-subscribe --zip-file fileb://quehook-querysubscribe.zip --region us-east-1
+
+go build -ldflags "-X main.HANDLER=UNSUBSCRIBE" -l queryunsubscribe
+zip quehook-queryunsubscribe.zip queryunsubscribe
+aws lambda update-function-code --function-name quehook-subscription-unsubscribe --zip-file fileb://quehook-queryunsubscribe.zip --region us-east-1
--- main.go
+++ main.go
@@ -1,12 +1,42 @@
 package main
 
 import (
+	"errors"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+
+	"github.com/forstmeier/quehook/query"
+	"github.com/forstmeier/quehook/storage"
+	"github.com/forstmeier/quehook/subscription"
+	"github.com/forstmeier/quehook/table"
 )
 
-func starter() (events.APIGatewayProxyResponse, error) {
-	return events.APIGatewayProxyResponse{}, nil
+// HANDLER allows for build-time starter configuration
+var HANDLER string
+
+func starter(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	s := storage.New()
+	t := table.New()
+
+	switch HANDLER {
+	case "CREATE":
+		return query.Create(req, t, s)
+	case "RUN":
+		return query.Run(s, t)
+	case "DELETE":
+		return query.Delete(req, t, s)
+	case "SUBSCRIBE":
+		return subscription.Subscribe(req, t)
+	case "UNSUBSCRIBE":
+		return subscription.Unsubscribe(req, t)
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode:      500,
+		Body:            "requested lambda type not available",
+		IsBase64Encoded: false,
+	}, errors.New("requested lambda type not available")
 }
 
 func main() {
